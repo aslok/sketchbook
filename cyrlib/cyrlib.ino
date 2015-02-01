00e241841dbcfaa41cdc953f23762366fadb1df9
Fust Vitaliy <aslok.zp@gmail.com>
2015-02-01 17:54:18 +0200

    Current state

--- cyrlib/cyrlib.ino
+++ cyrlib/cyrlib.ino
@@ -1,7 +1,3 @@
-// Sketch uses 8 988 bytes (29%) of program storage space. Maximum is 30 720 bytes.
-// Global variables use 245 bytes (11%) of dynamic memory, leaving 1 803 bytes for local variables. Maximum is 2 048 bytes.
-
-
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 #include <CyrI2c.h>
