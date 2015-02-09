35b84052fa734ec59cc0fb60a46277f960256884
Fust Vitaliy <aslok.zp@gmail.com>
2015-02-10 01:11:55 +0200

    UTF8 to CP1251 - library for encoding conversion

---
+++ libraries/utf8_cp1251/utf8_cp1251.h
@@ -0,0 +1,15 @@
+/*
+modified 09.02.2015
+by Fust Vitaliy
+with Arduino 1.5.8 (tested on Arduino Nano)
+*/
+
+#ifndef _UTF8_CP1251_H
+#define _UTF8_CP1251_H
+
+#include <Arduino.h>
+
+int utf8_to_cp1251(const char* utf8, char* windows1251, int n);
+int utf8_to_cp1251(const __FlashStringHelper* utf8_ptr, char* windows1251, int n);
+
+#endif
