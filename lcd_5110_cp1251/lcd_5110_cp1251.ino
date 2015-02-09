5c452f969b79e97c2b6206de632ef3858b4536f1
Fust Vitaliy <aslok.zp@gmail.com>
2015-02-10 01:13:19 +0200

    Adafruit - LCD Nokia 5110 library, cyrillic example

---
+++ lcd_5110_cp1251/lcd_5110_cp1251.ino
@@ -0,0 +1,28 @@
+#include "SPI.h"
+#include "Adafruit_GFX.h"
+#include "Adafruit_PCD8544.h"
+#include "utf8_cp1251.h"
+
+// D3 - LCD 1 - reset (RST)
+// D4 - LCD 2 - chip select (CS)
+// D5 - LCD 3 - Data/Command select (D/C)
+// D6 - LCD 4 - Serial data out (DIN)
+// D7 - LCD 5 - Serial clock out (SCLK)
+Adafruit_PCD8544 display = Adafruit_PCD8544(3, 4, 5, 6, 7);
+
+void setup() {
+  display.begin();
+  display.setContrast(51);
+  //display.setTextSize(2);
+  char cp1251[29];
+  utf8_to_cp1251(F("Привіт сонечко"), cp1251, 29);
+  display.println(cp1251);
+  utf8_to_cp1251(F("Доброго дня =)"), cp1251, 29);
+  display.println(cp1251);
+  display.println(F("\n    \xAB \xA4 \xBB"));
+  display.display();
+}
+
+
+void loop() {
+}
