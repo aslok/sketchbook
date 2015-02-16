75a7f2dc3717b9250b2d668474c97dc92e7256af
Fust Vitaliy <aslok.zp@gmail.com>
2015-02-16 17:12:17 +0200

    LCD 5110 GIF - version 2.1

--- lcd_5110_gif/lcd_5110_gif.ino
+++ lcd_5110_gif/lcd_5110_gif.ino
@@ -1,6 +1,6 @@
 /*
-Sketch uses 29 424 bytes (95%) of program storage space. Maximum is 30 720 bytes.
-Global variables use 596 bytes (29%) of dynamic memory, leaving 1 452 bytes for local variables. Maximum is 2 048 bytes.
+Sketch uses 29 416 bytes (95%) of program storage space. Maximum is 30 720 bytes.
+Global variables use 602 bytes (29%) of dynamic memory, leaving 1 446 bytes for local variables. Maximum is 2 048 bytes.
 */
 
 #include "SPI.h"
@@ -17,48 +17,67 @@ Adafruit_PCD8544 display = Adafruit_PCD8544(3, 4, 5, 6, 7);
 
 #include "imgs.c"
 
-const byte ms = 75;
-byte current = 0;
+// Продолжительность анимации, мс
+const word full_time = 9360;
+
+typedef struct subtitle {
+  char str[29];
+  byte start;
+  byte finish;
+};
+
+// Старт и финиш - область таймлайна для отображения, от 0 до 100
+// Например - старт 4% от 9360 мс, финиш 13% от 9360 мс
+const subtitle PROGMEM st[] = {
+  { " ЗДРАВСТВУЙТЕ!",  4, 13 },
+  { "     ПАРУ СЛОВ", 15, 23 },
+  { "    О ПОЛИТИКЕ", 23, 31 },
+  { "        БЛЯ   ", 31, 38 },
+  { " ЗАЦЕНИ ПРИКОЛ", 38, 50 },
+  { "      ПОЛИТИКА", 52, 58 },
+  { "     ПОЛИТИКА!", 63, 75 },
+  { " ПОЙДЕМ ДАЛЬШЕ", 79, 88 },
+};
+byte st_cnt = sizeof(st) / sizeof(subtitle);
+byte cur_img = 0;
+unsigned long mill;
 
 void setup() {
   display.begin();
   display.setContrast(51);
+  mill = millis();
 }
 
-void say(const __FlashStringHelper* str){
+inline void say(const __FlashStringHelper* str){
   char cp1251[29];
   display.fillRect(0, 0, LCDWIDTH, 8, WHITE);
   utf8_to_cp1251(str, cp1251, 29);
   display.setCursor(0, 0);
-  display.println(cp1251);
+  display.print(cp1251);
 }
 
 void loop() {
-  if (current == imgs_cnt){
-    current = 0;
+  if (cur_img == imgs_cnt){
+    cur_img = 0;
   }
-  display.drawBitmap(0, 8, imgs[current], 84, 40, BLACK, WHITE);
-  if (      current > 2  && current < 7){
-    say(F(" ЗДРАВСТВУЙТЕ!"));
-  }else if (current > 8  && current < 12){
-    say(F("     ПАРУ СЛОВ"));
-  }else if (current > 12 && current < 16){
-    say(F("    О ПОЛИТИКЕ"));
-  }else if (current > 16 && current < 20){
-    say(F("        БЛЯ   "));
-  }else if (current > 20 && current < 26){
-    say(F(" ЗАЦЕНИ ПРИКОЛ"));
-  }else if (current > 27 && current < 31){
-    say(F("      ПОЛИТИКА"));
-  }else if (current > 33 && current < 39){
-    say(F("     ПОЛИТИКА!"));
-  }else if (current > 41 && current < 46){
-    say(F(" ПОЙДЕМ ДАЛЬШЕ"));
-  }else{
-    say(F("              "));
+  display.drawBitmap(0, 8, imgs[cur_img], 84, 40, BLACK, WHITE);
+  byte st_num;
+  for (st_num = 0; st_num < st_cnt; st_num++){
+    if (
+      cur_img > pgm_read_byte_near(&st[st_num].start)  * imgs_cnt / 100 &&
+      cur_img < pgm_read_byte_near(&st[st_num].finish) * imgs_cnt / 100
+    ){
+      say((__FlashStringHelper*) &st[st_num].str);
+      break;
+    }
+  }
+  if (st_num == st_cnt){
+    display.fillRect(0, 0, LCDWIDTH, 8, WHITE);
   }
   display.display();
-  delay(ms);
-  current++;
-}
+  cur_img++;
 
+  unsigned long cur_mil;
+  while ((cur_mil = millis()) - mill < full_time / imgs_cnt);
+  mill = cur_mil;
+}
