598a6ad81a404c21d634379f36d61b9ec2545683
Vitaliy Fust <aslok.zp@gmail.com>
2016-08-28 00:25:21 +0300

    Small changes

--- watch/watch.ino
+++ watch/watch.ino
@@ -1,5 +1,5 @@
 // Флаг для отладки через порт
-const boolean debug = true;
+boolean debug = false;
 
 const byte left[5] = {
   9, 8, 7, 6, 5
@@ -20,20 +20,37 @@ const byte right_bottom[4] = {
   10, 11, 12, 13
 };
 
+const byte button_1_pin = A6;
+const byte button_2_pin = A7;
+
+boolean b1 = false;
+boolean b2 = false;
+boolean b3 = false;
+boolean button1 = false;
+boolean button2 = false;
+boolean button3 = false;
+boolean cmd_mode_1 = false;
+boolean cmd_mode_2 = false;
+boolean cmd_mode_3 = false;
+unsigned long cmd_start_1 = 0;
+unsigned long cmd_start_2 = 0;
+unsigned long cmd_start_3 = 0;
+
+unsigned long ms;
+unsigned long ms_prev;
+
+byte month;
+byte day;
+byte hour;
+
 #include <Wire.h>
 #include "RTClib.h"
 
 RTC_DS1307 rtc;
 
-
-#include <SFE_BMP180.h>
-#include <Wire.h>
-
-SFE_BMP180 pressure;
-
-#define ALTITUDE 0.0
-
 void setup() {
+  debug = true;
+
   pinMode(5, INPUT_PULLUP);
   pinMode(6, OUTPUT);
   pinMode(7, OUTPUT);
@@ -59,6 +76,9 @@ void setup() {
   digitalWrite(A2, LOW);
   digitalWrite(A3, LOW);
 
+  pinMode(button_1_pin, INPUT_PULLUP);
+  pinMode(button_2_pin, INPUT_PULLUP);
+
   if (debug) {
     Serial.begin(57600);
   }
@@ -70,88 +90,59 @@ void setup() {
 #endif
   rtc.begin();
 
-  pressure.begin();
+  ms_prev = 0;
 }
 
 void loop() {
+  ms = millis();
+
+  read_buttons();
+
+  if (ms - ms_prev < 1000 && !button1 && !button2 && !button3){
+    return;
+  }
+
+  ms_prev = ms;
+
   DateTime now = rtc.now();
-
-  randomSeed(now.unixtime());
-
-  
-  Serial.print(now.year(), DEC);
-  Serial.print('/');
-  Serial.print(now.month(), DEC);
-  Serial.print('/');
-  Serial.print(now.day(), DEC);
-  Serial.print(' ');
-  Serial.print(now.hour(), DEC);
-  Serial.print(':');
-  Serial.print(now.minute(), DEC);
-  Serial.print(':');
-  Serial.print(now.second(), DEC);  
-  Serial.println();
-
-  byte hour = now.hour();
-  if (hour > 12){
-    hour %= 12;
-  }else if (!hour){
-    hour = 12;
+  if (debug) {
+    Serial.print(now.year(), DEC);
+    Serial.print('/');
+    Serial.print(now.month(), DEC);
+    Serial.print('/');
+    Serial.print(now.day(), DEC);
+    Serial.print(' ');
+    Serial.print(now.hour(), DEC);
+    Serial.print(':');
+    Serial.print(now.minute(), DEC);
+    Serial.print(':');
+    Serial.print(now.second(), DEC);  
+    Serial.println();
   }
-  byte min = now.minute();
 
-  show_left(hour);
-  show_right(min);
+  if (button1) {
+    show_snow();
+  }else if (button2) {
+    randomSeed(now.unixtime());
+    show_wizard();
+  }else if (button3) {
+    month = now.month();
+    day = now.day();
 
-  delay(5000);
-
-  byte month = now.month();
-  byte day = now.day();
-
-  show_left(month);
-  show_right(day);
-
-  delay(5000);
-
-  char status;
-  double T, P;
-  
-  status = pressure.startTemperature();
-  if (status != 0){
-    delay(status);
-    status = pressure.getTemperature(T);
-    if (status != 0){
-      T -= 3.5;
-      Serial.print("temperature: ");
-      Serial.print(T, 2);
-      Serial.println(" deg C");
-
-      byte temp = T;
-      show_left_one(temp);
-
-      status = pressure.startPressure(3);
-      if (status != 0){
-        delay(status);
-        status = pressure.getPressure(P, T);
-        P -= 235;
-        if (status != 0){
-
-          Serial.print("absolute pressure: ");
-          Serial.print(P, 2);
-          Serial.println(" mb");
-
-          byte pres = P - 700;
-          show_right_one(pres);
-        }
-      }
+    show_left(month);
+    show_right(day);
+  }else{
+    hour = now.hour();
+    if (hour > 12){
+      hour %= 12;
+    }else if (!hour){
+      hour = 12;
     }
+    byte min = now.minute();
+
+    show_left(hour);
+    show_right(min);
   }
-
-  delay(5000);
-
-  show_snow();
-
-  show_wizard();
 }
 
 void show_snow(){
@@ -203,8 +194,10 @@ void show_wizard(){
       do{
         exists = false;
         next = random(0, 12);
-        Serial.print("random = ");
-        Serial.println(next);
+        if (debug) {
+          Serial.print("random = ");
+          Serial.println(next);
+        }
         for (byte f3 = 0; f3 < points; f3++){
           if (on[f3] == 127){
             break;
@@ -215,12 +208,18 @@ void show_wizard(){
           exists = true;
           break;
         }
-        Serial.print("exists = ");
-        Serial.println(exists);
+        if (debug) {
+          Serial.print("exists = ");
+          Serial.println(exists);
+        }
       }while (exists);
       on[f2] = next;
-      Serial.print("next = ");
-      Serial.println(next);
+      if (debug) {
+        Serial.print("next = ");
+        Serial.println(next);
+        Serial.print("next > 4 ? right[next - 5] : left[next] = ");
+        Serial.println(next > 4 ? right[next - 5] : left[next]);
+      }
       digitalWrite(next > 4 ? right[next - 5] : left[next], HIGH);
     }
     delay(50);
@@ -228,65 +227,85 @@ void show_wizard(){
 }
 
 void show_left_one(byte digit){
-  Serial.print("LEFT ONE - ");
-  Serial.print(digit, DEC);
-  Serial.println();
-  for (byte f = 0; f < 5; f++){
-    Serial.print(bitRead(digit, f) ? 1 : 0, DEC);
+  if (debug) {
+    Serial.print("LEFT ONE - ");
+    Serial.print(digit, DEC);
     Serial.println();
+  }
+  for (byte f = 0; f < 5; f++){
+    if (debug) {
+      Serial.print(bitRead(digit, f) ? 1 : 0, DEC);
+      Serial.println();
+    }
     digitalWrite(left[f], bitRead(digit, f) ? HIGH : LOW);
   }
 }
 
 void show_right_one(byte digit){
-  Serial.print("RIGHT ONE - ");
-  Serial.print(digit, DEC);
-  Serial.println();
-  for (byte f = 0; f < 7; f++){
-    Serial.print(bitRead(digit, f) ? 1 : 0, DEC);
+  if (debug) {
+    Serial.print("RIGHT ONE - ");
+    Serial.print(digit, DEC);
     Serial.println();
+  }
+  for (byte f = 0; f < 7; f++){
+    if (debug) {
+      Serial.print(bitRead(digit, f) ? 1 : 0, DEC);
+      Serial.println();
+    }
     digitalWrite(right[f], bitRead(digit, f) ? HIGH : LOW);
   }
 }
 
 void show_left(byte digit){
-  Serial.print("LEFT - ");
-  Serial.print(digit, DEC);
-  Serial.println();
+  if (debug) {
+    Serial.print("LEFT - ");
+    Serial.print(digit, DEC);
+    Serial.println();
+  }
   int digits[2];
   int digits_count = factor_digits(digit, digits, 2);
   if (digits_count < 2){
     digits[1] = 0;
   }
   for (byte f = 0; f < 4; f++){
-    Serial.print(bitRead(digits[0], f) ? 1 : 0, DEC);
-    Serial.println();
+    if (debug) {
+      Serial.print(bitRead(digits[0], f) ? 1 : 0, DEC);
+      Serial.println();
+    }
     digitalWrite(left_bottom[f], bitRead(digits[0], f) ? HIGH : LOW);
   }
   for (byte f = 0; f < 1; f++){
-    Serial.print(bitRead(digits[1], f) ? 1 : 0, DEC);
-    Serial.println();
+    if (debug) {
+      Serial.print(bitRead(digits[1], f) ? 1 : 0, DEC);
+      Serial.println();
+    }
     digitalWrite(left_top[f], bitRead(digits[1], f) ? HIGH : LOW);
   }
 }
 
 void show_right(byte digit){
-  Serial.print("RIGHT - ");
-  Serial.print(digit, DEC);
-  Serial.println();
+  if (debug) {
+    Serial.print("RIGHT - ");
+    Serial.print(digit, DEC);
+    Serial.println();
+  }
   int digits[2];
   int digits_count = factor_digits(digit, digits, 2);
   if (digits_count < 2){
     digits[1] = 0;
   }
   for (byte f = 0; f < 4; f++){
-    Serial.print(bitRead(digits[0], f) ? 1 : 0, DEC);
-    Serial.println();
+    if (debug) {
+      Serial.print(bitRead(digits[0], f) ? 1 : 0, DEC);
+      Serial.println();
+    }
     digitalWrite(right_bottom[f], bitRead(digits[0], f) ? HIGH : LOW);
   }
   for (byte f = 0; f < 3; f++){
-    Serial.print(bitRead(digits[1], f) ? 1 : 0, DEC);
-    Serial.println();
+    if (debug) {
+      Serial.print(bitRead(digits[1], f) ? 1 : 0, DEC);
+      Serial.println();
+    }
     digitalWrite(right_top[f], bitRead(digits[1], f) ? HIGH : LOW);
   }
 }
@@ -303,4 +322,65 @@ int factor_digits(int num, int *digits, int limit){
     limit--;
   }
   return num ? 0 : digits_count;
+}
+
+
+void read_buttons(){
+  b1 = !analogRead(button_1_pin);
+  b2 = !analogRead(button_2_pin);
+  b3 = analogRead(button_1_pin) > 100 && analogRead(button_1_pin) < 150;
+  button1 = false;
+  button2 = false;
+  button3 = false;
+
+  if (debug) {
+    Serial.print("button #3 = ");
+    Serial.println(analogRead(button_1_pin));
+  }
+
+  if (!b1){
+    cmd_mode_1 = true;
+  }
+  if (!b2){
+    cmd_mode_2 = true;
+  }
+  if (!b3){
+    cmd_mode_3 = true;
+  }
+
+  if (cmd_mode_1 && b1){
+    cmd_mode_1 = false;
+    cmd_start_1 = ms;
+  }
+  if (cmd_mode_2 && b2){
+    cmd_mode_2 = false;
+    cmd_start_2 = ms;
+  }
+  if (cmd_mode_3 && b3){
+    cmd_mode_3 = false;
+    cmd_start_3 = ms;
+  }
+
+  if (cmd_mode_1 && cmd_start_1 && ms - cmd_start_1 <= 50){
+    cmd_start_1 = 0;
+  } 
+  if (cmd_mode_2 && cmd_start_2 && ms - cmd_start_2 <= 50){
+    cmd_start_2 = 0;
+  }
+  if (cmd_mode_3 && cmd_start_3 && ms - cmd_start_3 <= 50){
+    cmd_start_3 = 0;
+  }
+
+  if (!cmd_mode_1 && cmd_start_1 && (int) (ms - cmd_start_1) > 50 && b1){
+    cmd_start_1 = cmd_start_1 + 250;
+    button1 = true;
+  }
+  if (!cmd_mode_2 && cmd_start_2 && (int) (ms - cmd_start_2) > 50 && b2){
+    cmd_start_2 = cmd_start_2 + 250;
+    button2 = true;
+  }
+  if (!cmd_mode_3 && cmd_start_3 && (int) (ms - cmd_start_3) > 50 && b3){
+    cmd_start_3 = cmd_start_3 + 250;
+    button3 = true;
+  }
 }
