93d69ba9ad903406c1a77c40a9baf9694aa3a511
Vitaliy Fust <aslok.zp@gmail.com>
2017-06-07 17:28:41 +0300

    Modified Dimmer project, added library TimerOne

--- dimmer/dimmer.ino
+++ dimmer/dimmer.ino
@@ -1,10 +1,19 @@
 boolean debug = false;
+boolean debug_blink = false;
 
 #include "EEPROM.h"
+#include "TimerOne.h"
 
 const byte button_1_pin = A1;
 const byte button_2_pin = A0;
 const byte buttons_low_pin = 10;
+
+const byte cros_zero_low_pin = 8;
+const byte cros_zero_high_pin = 7;
+const byte cros_zero_pin = 2;
+volatile unsigned long cros_zero_us = 0;
+volatile boolean interrupt_signal_skip = true;
+int period = 0;
 
 boolean b1 = false;
 boolean b2 = false;
@@ -47,23 +56,36 @@ boolean blink = false;
 unsigned long blink_ms = 0;
 boolean blink_prev = false;
 
-byte signal;
+byte signal = 0;
 int signal_prev = -1;
 
 void setup(){
   //debug = true;
+  //debug_blink = true;
   if (debug){
     Serial.begin(57600);
   }
+
+  Timer1.initialize();
+  Timer1.attachInterrupt(interrupt_signal_stop);
+  Timer1.stop();
+
   pinMode(button_1_pin, INPUT_PULLUP);
   pinMode(button_2_pin, INPUT_PULLUP);
   digitalWrite(buttons_low_pin, LOW);
   pinMode(buttons_low_pin, OUTPUT);
 
+  digitalWrite(cros_zero_low_pin, LOW);
+  pinMode(cros_zero_low_pin, OUTPUT);
+  digitalWrite(cros_zero_high_pin, HIGH);
+  pinMode(cros_zero_high_pin, OUTPUT);
+  pinMode(cros_zero_pin, INPUT);
+  attachInterrupt(0, interrupt_cros_zero, RISING);
+
   digitalWrite(signal_pin, LOW);
   pinMode(signal_pin, OUTPUT);
-  TCCR1B = TCCR1B & B11111000 | B00000001;
-  TCCR2B = TCCR2B & B11111000 | B00000001;
+  TCCR1B = (TCCR1B & B11111000) | B00000001;
+  TCCR2B = (TCCR2B & B11111000) | B00000001;
 
   analogWrite(m, 255 - m_val);
   digitalWrite(r, LOW);
@@ -81,6 +103,11 @@ void setup(){
 
 void loop(){
   ms = millis();
+
+  if (ms < blink_ms){
+    blink_ms = 0;
+    cros_zero_us = 0;
+  }
 
   read_buttons();
 
@@ -134,12 +161,20 @@ void loop(){
       EEPROM.write(0, step);
       EEPROM.write(1, step_part);
     }
+    if (debug && debug_blink){
+      Serial.print("step = ");
+      Serial.println(step, DEC);
+      Serial.print("step_part = ");
+      Serial.println(step_part, DEC);
+    }
     blink_prev = blink;
     if (blink){
       analogWrite(r, steps[step][0]);
       analogWrite(g, steps[step][1]);
       analogWrite(b, steps[step][2]);
-      if (debug){
+      if (debug && debug_blink){
+        Serial.print("ms = ");
+        Serial.println(ms);
         Serial.print("r = ");
         Serial.println(steps[step][0]);
         Serial.print("g = ");
@@ -152,22 +187,54 @@ void loop(){
       analogWrite(g, 0);
       analogWrite(b, 0);
     }
-    if (debug){
-      Serial.println((15 / 23.0) * ((step - 1) * 3 + step_part + 1), DEC);
+    uint16_t signal_val = step > 0 || step_part > 0 ? ceil(pow((6.248 / 24.0) * ((step - 1) * 3 + step_part + 1), 3)) : 0;
+    if (signal_val && step >= 1){
+      if (step == 1 && step_part){
+        signal_val += step_part;
+      }
+      if (step > 1){
+        signal_val += 2;
+      }
+      signal_val += 10;
     }
-    //signal = step > 0 || step_part > 0 ? (2 + ((step - 1) * 3 + step_part) * 11) : 0;
-    signal = step > 0 || step_part > 0 ? (byte) ceil(pow((15 / 23.0) * ((step - 1) * 3 + step_part + 1), 2)) : 0;
-    if (signal == 245){
-      signal = 255;
-    }
-    if (debug){
-      Serial.println(signal, DEC);
-    }
+    signal = signal_val > 255 ? 255 : signal_val;
     if (signal_prev != signal){
       signal_prev = signal;
-      analogWrite(signal_pin, signal);
+      period = 9000 - 9000.0 / 255 * signal;
+      if (debug){
+        Serial.print("signal_val = ");
+        Serial.println(signal_val, DEC);
+        Serial.print("signal = ");
+        Serial.println(signal, DEC);
+        Serial.print("period = ");
+        Serial.println(period, DEC);
+      }
     }
   }
+}
+
+void interrupt_cros_zero(){
+  unsigned long us = micros();
+  if (us - cros_zero_us < 2500){
+    return;
+  }
+  cros_zero_us = us;
+  if (signal < 255){
+    digitalWrite(signal_pin, LOW);
+  }
+  if (signal){
+    Timer1.setPeriod(period + 1000);
+    Timer1.start();
+  }
+}
+
+void interrupt_signal_stop(){
+  interrupt_signal_skip = !interrupt_signal_skip;
+  if (interrupt_signal_skip){
+    return;
+  }
+  digitalWrite(signal_pin, HIGH);
+  Timer1.stop();
 }
 
 void read_buttons(){
