db1c2c0ae91c4a2d47f1c62b67815068deea7b29
Vitaliy Fust <aslok.zp@gmail.com>
2017-10-11 18:59:56 +0300

    Changes for new version of aRTClib library

--- lcd_7seg_x_4_cyr_artclib/lcd_7seg_x_4_cyr_artclib.ino
+++ lcd_7seg_x_4_cyr_artclib/lcd_7seg_x_4_cyr_artclib.ino
@@ -2,7 +2,8 @@
  * lcd_7seg_x_4_cyr_artclib.ino
  * Clock
  *
- * modified 19.08.2017
+ * created 19.08.2017
+ * modified 11.10.2017
  * by Fust Vitaliy Vitaliy Fust <aslok.zp@gmail.com>
  * with Arduino 1.8.3 (tested on Arduino Pro Mini 168 5V)
  *
@@ -22,8 +23,8 @@
  * MA 02110-1301, USA.
  *
  *
-Sketch uses 13,474 bytes (94.0%) of program storage space. Maximum is 14,336 bytes.
-Global variables use 556 bytes (54.3%) of dynamic memory, leaving 468 bytes for local variables. Maximum is 1,024 bytes.
+Sketch uses 13,460 bytes (93.9%) of program storage space. Maximum is 14,336 bytes.
+Global variables use 586 bytes (57.2%) of dynamic memory, leaving 438 bytes for local variables. Maximum is 1,024 bytes.
  */
 boolean debug = false;
 
@@ -52,13 +53,20 @@ char buffer_incom[21];
 byte buffer_incom_pos = 0;
 boolean buffer_incom_done = false;
 
-// Mode: normal, set time, set adjust, set unixtime
+// Также есть команды:
+// read - чтение значений из памяти
+// applied - сброс даты ежедневной поправки на 0
 enum mode_type {
+  // Вывод текущего времени
   normal,
-  set,
-  adjust,
-  unixtime,
-  time
+  // Поправка времени на верное
+  correct,
+  // Ввод ежедневной поправки вручную
+  amendment,
+  // Ввод даты последней ручной поправки вручную
+  corrected,
+  // Ввод верной даты вручную
+  adjust
 };
 mode_type mode = normal;
 
@@ -85,13 +93,14 @@ void setup(){
 void loop(){
   us = micros();
 
-  if (us - us_prev > 1000000){
+  if (us - us_prev > 1e6){
     us_prev = us;
     rtc->update();
 
     byte hour = rtc->date.hour() % 12;
     sprintf(buffer, "%d%s%02d", hour ? hour : 12, rtc->date.second() % 2 ? "." : "", rtc->date.minute());
     lcd->print(buffer);
+    // Вывод текущего времени
     if (mode == normal){
       char buffer_tmp[21];
       sprintf(buffer_tmp,
@@ -110,10 +119,6 @@ void loop(){
         buffer_incom[buffer_incom_pos] = '\0';
         buffer_incom_pos = 0;
         buffer_incom_done = true;
-        if (debug){
-          Serial.print(F("Received: "));
-          Serial.println(buffer_incom);
-        }
         break;
       default:
         buffer_incom[buffer_incom_pos++] = incom_byte;
@@ -122,29 +127,37 @@ void loop(){
 
   if (buffer_incom_done){
     if (mode == normal) {
-      if (!strcmp(buffer_incom, "set")){
-        mode = set;
-        Serial.println(F("SET: Please enter"));
+      // Поправка времени на верное
+      if (!strcmp(buffer_incom, "correct")){
+        mode = correct;
+        Serial.println(F("CORRECT: Please enter"));
+      // Ввод ежедневной поправки вручную
+      }else if (!strcmp(buffer_incom, "amendment")){
+        mode = amendment;
+        Serial.println(F("AMENDMENT: Please enter"));
+      // Ввод даты последней ручной поправки вручную
+      }else if (!strcmp(buffer_incom, "corrected")){
+        mode = corrected;
+        Serial.println(F("CORRECTED: Please enter"));
+      // Чтение значений из памяти
+      }else if (!strcmp(buffer_incom, "read")){
+        Serial.print(F("READ: Corrected "));
+        Serial.println(rtc->corrected_read());
+        Serial.print(F("READ: Amendment "));
+        Serial.println(rtc->amendment_read(), 4);
+        Serial.print(F("READ: Applied "));
+        Serial.println(rtc->applied_read());
+      // Сброс даты ежедневной поправки на 0
+      }else if (!strcmp(buffer_incom, "applied")){
+        rtc->applied_write(0);
+        Serial.println(F("APPLIED: 0"));
+      // Ввод верной даты вручную
       }else if (!strcmp(buffer_incom, "adjust")){
         mode = adjust;
         Serial.println(F("ADJUST: Please enter"));
-      }else if (!strcmp(buffer_incom, "unixtime")){
-        mode = unixtime;
-        Serial.println(F("UNIXTIME: Please enter"));
-      }else if (!strcmp(buffer_incom, "read")){
-        Serial.print(F("READ: Unixtime "));
-        Serial.println(rtc->unixtime_read());
-        Serial.print(F("READ: Adjust "));
-        printFloat(rtc->adjust_read(), 4);
-        Serial.println();
-      }else if (!strcmp(buffer_incom, "last")){
-        rtc->last_write(0);
-        Serial.println(F("LAST ADJUST: 0"));
-      }else if (!strcmp(buffer_incom, "time")){
-        mode = time;
-        Serial.println(F("TIME: Please enter"));
       }
-    }else if (mode == set){
+    // Поправка времени на верное
+    }else if (mode == correct){
       mode = normal;
       char delim[] = " :.";
       byte day = atoi(strtok(buffer_incom, delim));
@@ -155,42 +168,20 @@ void loop(){
       byte second = atoi(strtok(NULL, delim));
       // Правильное время:
       DateTime new_time = DateTime(year, month, day, hour, minute, second);
-      // Прошло столько секунд с момента последней коррекции (например 89349):
-      long time_elapsed = new_time.unixtime() - rtc->unixtime_read();
-      // Такая разница в секундах с правильным временем (например -2):
-      long time_diff = new_time.unixtime() - rtc->date.unixtime();
-      // Новая поправка
-      //     -12.9998                     86400           79755             -12                       0
-      float new_adjust = (time_diff ? (3600.0 * 24 / (time_elapsed / (float) time_diff)) : 0) + rtc->adjust_read();
-      if (debug){
-        Serial.print(F("SET: Old adjust value "));
-        printFloat(rtc->adjust_read(), 4);
-        Serial.println();
-        Serial.print(F("SET: Elapsed time "));
-        Serial.println(time_elapsed);
-        Serial.print(F("SET: Old timestamp "));
-        Serial.println(rtc->date.unixtime());
-        Serial.print(F("SET: New timestamp "));
-        Serial.println(new_time.unixtime());
-        Serial.print(F("SET: Time diff "));
-        Serial.println(time_diff);
-        Serial.print(F("SET: Adjust "));
-        printFloat(new_adjust, 4);
-        Serial.println();
-      }
-      rtc->unixtime_write(new_time.unixtime());
-      rtc->adjust_write(new_adjust);
-      rtc->set(new_time);
+      rtc->correct(new_time);
+    // Ввод ежедневной поправки вручную
+    }else if (mode == amendment){
+      mode = normal;
+      float new_amendment = atof(buffer_incom);
+      rtc->amendment_write(new_amendment);
+    // Ввод даты последней ручной поправки вручную
+    }else if (mode == corrected){
+      mode = normal;
+      unsigned long new_corrected = atol(buffer_incom);
+      rtc->corrected_write(new_corrected);
+    // Ввод верной даты вручную
     }else if (mode == adjust){
       mode = normal;
-      float new_adjust = atof(buffer_incom);
-      rtc->adjust_write(new_adjust);
-    }else if (mode == unixtime){
-      mode = normal;
-      unsigned long new_unixtime = atol(buffer_incom);
-      rtc->unixtime_write(new_unixtime);
-    }else if (mode == time){
-      mode = normal;
       char delim[] = " :.";
       byte day = atoi(strtok(buffer_incom, delim));
       byte month = atoi(strtok(NULL, delim));
@@ -200,55 +191,10 @@ void loop(){
       byte second = atoi(strtok(NULL, delim));
       // Правильное время:
       DateTime new_time = DateTime(year, month, day, hour, minute, second);
-      rtc->set(new_time);
+      rtc->adjust(new_time);
     }
     buffer_incom_done = false;
   }
 
   lcd->update();
-}
-
-void printFloat(float value, int places){
-  int digit;
-  float tens = 0.1;
-  int tenscount = 0;
-  int i;
-  float tempfloat = value;
-  float d = 0.5;
-  if (value < 0){
-    d *= -1.0;
-  }
-  for (i = 0; i < places; i++){
-    d /= 10.0;
-  }
-  tempfloat += d;
-  if (value < 0){
-    tempfloat *= -1.0;
-  }
-  while ((tens * 10.0) <= tempfloat){
-    tens *= 10.0;
-    tenscount += 1;
-  }
-  if (value < 0){
-    Serial.print('-');
-  }
-  if (tenscount == 0){
-    Serial.print(0, DEC);
-  }
-  for (i = 0; i < tenscount; i++){
-    digit = (int) (tempfloat / tens);
-    Serial.print(digit, DEC);
-    tempfloat = tempfloat - ((float) digit * tens);
-    tens /= 10.0;
-  }
-  if (places <= 0){
-    return;
-  }
-  Serial.print('.');
-  for (i = 0; i < places; i++){
-    tempfloat *= 10.0;
-    digit = (int) tempfloat;
-    Serial.print(digit, DEC);
-    tempfloat = tempfloat - (float) digit;
-  }
 }
